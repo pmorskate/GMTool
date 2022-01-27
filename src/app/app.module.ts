db28b001f8b08947af4e06145e134ac9f32a4ba2
Pom <74912005+pmorskate@users.noreply.github.com>
2022-01-27 15:20:55 +0100

    toevoegen statische referentie
    Alleen vaste waardes op de site. Nog niet flexibel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ArticleComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/article.ts
@@ -0,0 +1,8 @@
+export interface Article {
+  id: number;
+  title: string;
+  header1: string;
+  content1: string;
+  header2: string;
+  content2: string;
+}
